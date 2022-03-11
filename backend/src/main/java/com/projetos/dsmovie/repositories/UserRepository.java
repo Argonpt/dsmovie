9be11050508a2445c642fd1d3e4e33790a8b8d7f
unknown <rui.correia1200@gmail.com>
2022-03-11 01:48:03 +0000

    Save score

---
+++ backend/src/main/java/com/projetos/dsmovie/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.projetos.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetos.dsmovie.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+
+}

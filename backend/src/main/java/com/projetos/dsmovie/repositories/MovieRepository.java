abb1e9c30710fcef3a9cc81f6c892a2477ed8368
unknown <rui.correia1200@gmail.com>
2022-03-11 01:05:44 +0000

    Find movies

---
+++ backend/src/main/java/com/projetos/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,13 @@
+package com.projetos.dsmovie.repositories;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetos.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+	
+
+}
